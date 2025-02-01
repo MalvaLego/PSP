629612b4076365243c01f8a3ab5e596f420b4cb8
MalvaLego <nataliacoronado555@gmail.com>
2025-02-01 16:03:22 +0100

    add ejer33_Butacas

--- UD3_serviciosComunicacion/src/ejer33_TicketsButacas/TicketServer.java
+++ UD3_serviciosComunicacion/src/ejer33_TicketsButacas/TicketServer.java
@@ -1,10 +1,98 @@
 package ejer33_TicketsButacas;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.*;
+
 public class TicketServer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {		
+		//DatagramPacket envio=null;
+		String[] butacas=new String[4];
+		for (int i=0;i<butacas.length;i++) {
+			butacas[i]="LIBRE";
+		}
+		Queue<String> colaButacas = new LinkedList<>();
+		
+		//Esperando Datagrama
+		System.out.println("Servidor esperando datagrama...");
+		DatagramPacket recibo;
+		DatagramSocket socket= new DatagramSocket(12345);
+		
+		while (true) {
+			byte[] buffer= new byte[1024];
+			recibo= new DatagramPacket(buffer,buffer.length); 
+			socket.receive(recibo);	
+			
+			String mensaje= new String(recibo.getData()).trim();
+			String[] recibido=mensaje.split(";");
 
+			if (recibido[0].equals("1")) {
+				mostrarButacas(butacas,colaButacas);
+			}else
+			if (recibido[0].equals("2")) {
+				reservarButacas(butacas,colaButacas,recibido[1]);
+			}else
+			if (recibido[0].equals("3")) {
+				anularButacas(butacas,colaButacas,recibido[1]);
+			}else
+			if (recibido[0].equals("4")) {
+				break;
+			}else {
+				System.out.println("Opción incorrecta");
+			}
+		}
+			
+		//Cerrar socket
+		System.out.println("Cerrando conexión...");
+		socket.close();
+		
 	}
+
+	private static void mostrarButacas(String[] butacas, Queue<String> colaButacas) {
+		for (int i=0;i<butacas.length;i++) {
+			System.out.println(i+1+"-"+butacas[i]);
+		}
+		
+		if (!colaButacas.isEmpty()) {
+			System.out.println("Reservas:");
+			int contador=0;
+			for (String cad: colaButacas) {
+				contador++;
+				System.out.println(contador+"."+cad);
+			}
+		}
+	}
+
+	private static void reservarButacas(String[] butacas, Queue<String> colaButacas,String nombre) {
+		for (int i=0;i<butacas.length;i++) {
+			if (butacas[i].equals("LIBRE")) {
+				butacas[i]=nombre;
+				System.out.println(nombre+" tiene asignada la butaca "+(i+1));
+				return;
+			}
+		}
+		
+		System.out.println("Lo siento ya no quedan butacas libres, pasa a la lista de reservas");
+		colaButacas.add(nombre);
+	}
+
+	private static void anularButacas(String[] butacas, Queue<String> colaButacas,String nombre) {
+		for (int i=0;i<butacas.length;i++) {
+			if (butacas[i].equals(nombre)) {				
+				System.out.println(nombre+" tenia asignada la butaca "+(i+1));
+				System.out.println("La butaca "+(i+1)+" queda libre.");
+				System.out.println("Se adjudica a la primera persona de los reservados que es : "+colaButacas.peek());
+				butacas[i]=colaButacas.poll();
+				return;
+			}
+		}
+		
+		System.out.println(nombre+" no tiene comprada ninguna butaca");
+	}
+
 
 }
--- UD3_serviciosComunicacion/src/ejer33_TicketsButacas/Usuario.java
+++ UD3_serviciosComunicacion/src/ejer33_TicketsButacas/Usuario.java
@@ -1,10 +1,78 @@
 package ejer33_TicketsButacas;
 
+import java.net.*;
+import java.util.*;
+
 public class Usuario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		DatagramSocket clienteSocket=null;
+		DatagramPacket recibo=null;
+		int puerto=12345;
+		InetAddress IPServidor=null;
+		int opcion=0;
+		
+		try {
+			//Datos del servidor al que enviar mensaje
+			IPServidor = InetAddress.getLocalHost();
+			
+			clienteSocket= new DatagramSocket(34567);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//Introducir datos por teclado
+		while (opcion!=4) {
+			System.out.println("==TICKETSERVER== ");
+			System.out.println("Introduce opcion: ");
+			opcion=sc.nextInt();
+			
+			if (opcion==1) {
+				enviarAccion(1,"mostrar",clienteSocket);
+			}
+			if (opcion==2) {
+				sc.nextLine();
+				System.out.println("Introduzca nombre para la reserva: ");
+				String nombre=sc.nextLine();
+				enviarAccion(2,nombre,clienteSocket);
+			}
+			if (opcion==3) {
+				sc.nextLine();
+				System.out.println("Introduzca nombre para anular reserva: ");
+				String nombre=sc.nextLine();
+				enviarAccion(3,nombre,clienteSocket);
+			}
+			if (opcion==4) {
+				enviarAccion(4,"salir",clienteSocket);
+			}
+		}
+		
 
+		clienteSocket.close();
+		
+		
+	}
+
+	private static void enviarAccion(int num,String nombre,DatagramSocket clienteSocket) {
+		String cadena=num+";"+nombre;
+		
+		//Convertir cadena en bytes
+		byte[] enviados= new byte[1024];
+		enviados=cadena.getBytes();
+		
+		try {
+			//Datos del servidor al que enviar mensaje y creamos el datagrama que irá al servidor 
+			int puerto=12345;
+			InetAddress IPServidor= InetAddress.getLocalHost();
+			//Creamos el datagrama que irá al servidor
+			DatagramPacket envio=new DatagramPacket(enviados,enviados.length,IPServidor,puerto);
+			//Realizamos el envio
+			clienteSocket.send(envio);	
+		}catch(Exception e) {
+			System.out.println("Error: "+e.getMessage());	
+		}
+		 
 	}
 
 }
