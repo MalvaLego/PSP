c3678cb6faef248ea4efaf89e24120659d80b6e1
MalvaLego <152992436+MalvaLego@users.noreply.github.com>
2025-02-03 15:44:35 +0100

    Update Alumno.java

--- UD3_serviciosComunicacion/src/ejer34_SocketsTCP/Alumno.java
+++ UD3_serviciosComunicacion/src/ejer34_SocketsTCP/Alumno.java
@@ -1,1 +1,76 @@
+package ejer34_SocketsTCP;
 
+import java.io.*;
+import java.util.*;
+
+public class Alumno implements Serializable{
+	private String nombre;
+	private int edad;
+	private String esRepartidor;
+	ArrayList<String> listaAsignaturas;
+
+	public Alumno(String nombre, int edad, String esRepartidor,ArrayList<String> listaAsignaturas) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.esRepartidor = esRepartidor;
+		this.listaAsignaturas=null;
+	}
+
+	public void addAsignatura(String nombre) {
+		listaAsignaturas.add(nombre);
+	}
+	
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+
+	public String getEsRepartidor() {
+		return esRepartidor;
+	}
+
+
+
+	public ArrayList<String> getListaAsignaturas() {
+		return listaAsignaturas;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
+
+	public void setEsRepartidor(String esRepartidor) {
+		this.esRepartidor = esRepartidor;
+	}
+
+
+
+	public void setListaAsignaturas(ArrayList<String> listaAsignaturas) {
+		this.listaAsignaturas = listaAsignaturas;
+	}
+
+
+
+	
+	
+	
+}
